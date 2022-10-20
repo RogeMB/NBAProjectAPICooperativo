9b1bb0918efa35829b81f4b26b97adbaf343c46d
davidgm26 <davidgama260402@gmail.com>
2022-10-20 14:26:37 +0200

    Comienzo de la landing

--- nbaProjectAPI/src/app/app.module.ts
+++ nbaProjectAPI/src/app/app.module.ts
@@ -2,14 +2,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ImportsMaterialModule } from 'src/imports-material/imports-material.module';
+import { LandingComponent } from './components/landing/landing.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    HeaderComponent,
+
+  
+
   ],
   imports: [
     AppRoutingModule,
-    ImportsMaterialModule
+    ImportsMaterialModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
