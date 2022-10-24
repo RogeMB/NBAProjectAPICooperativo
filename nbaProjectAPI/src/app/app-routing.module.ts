8405751eba535aa15b6ea5b63d6f31803887f04d
RogeMB <mohigefer.barog22@triana.salesianos.edu>
2022-10-24 13:30:36 +0200

    Cambios corregidos

--- nbaProjectAPI/src/app/app-routing.module.ts
+++ nbaProjectAPI/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './components/landing/landing.component';
 import { PlayerListComponent } from './components/player-list/player-list.component';
+import { TeamsComponent } from './components/teams/teams.component';
 
 const routes: Routes = [
   {path: 'players', component:PlayerListComponent},
+  {path: 'teams', component: TeamsComponent},
   {path: 'landing', component:LandingComponent},
   {path: '', redirectTo:"/landing",pathMatch:"full"}
 ];
